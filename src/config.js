b38a891af0d8b4426d2796738cafb9f445db1a98
Oron Nadiv <oron@nadiv.us>
2016-10-26 18:55:02 -0700

    skip redirect to ssl

--- src/config.js
+++ src/config.js
@@ -29,6 +29,8 @@ if (!config.redisUrl) {
   process.exit(1)
 }
 
+config.skipSSL = process.env.SKIP_SSL && process.env.SKIP_SSL.toUpperCase() === 'TRUE'
+
 config.uiUrl = process.env.UI_URL || (config.production ? null : 'http://localhost:3000')
 if (!config.uiUrl) {
   error(
